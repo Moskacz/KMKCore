be2661f23fc1873b31847813caeca9b0a4eec4f5
Michał Moskała <moskala.michal@gmail.com>
2018-06-30 19:19:03 +0200

    Setup targets for iOS and macOS

---
+++ KMKCoreiOS/KMKCoreiOS.h
@@ -0,0 +1,18 @@
+//
+//  KMKCoreiOS.h
+//  KMKCoreiOS
+//
+//  Created by Michal Moskala on 30.06.2018.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KMKCoreiOS.
+FOUNDATION_EXPORT double KMKCoreiOSVersionNumber;
+
+//! Project version string for KMKCoreiOS.
+FOUNDATION_EXPORT const unsigned char KMKCoreiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KMKCoreiOS/PublicHeader.h>
+
+
---
+++ KMKCoremacOS/KMKCoremacOS.h
@@ -0,0 +1,18 @@
+//
+//  KMKCoremacOS.h
+//  KMKCoremacOS
+//
+//  Created by Michal Moskala on 30.06.2018.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KMKCoremacOS.
+FOUNDATION_EXPORT double KMKCoremacOSVersionNumber;
+
+//! Project version string for KMKCoremacOS.
+FOUNDATION_EXPORT const unsigned char KMKCoremacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KMKCoremacOS/PublicHeader.h>
+
+
