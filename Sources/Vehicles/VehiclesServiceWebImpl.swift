0ba6482557e7687d52d84ce02ed944a2c1b2341a
Michał Moskała <moskala.michal@gmail.com>
2018-07-02 20:04:42 +0200

    Implemented VehiclesServiceWebImpl

--- Sources/Vehicles/VehiclesServiceWebImpl.swift
+++ Sources/Vehicles/VehiclesServiceWebImpl.swift
@@ -11,13 +11,28 @@ internal class VehiclesServiceWebImpl: VehiclesService {
     
     private let httpClient: HTTPClient
     private let mapper: Mapper<JSON, Vehicle>
+    private let vehiclesURL: URL
     
-    internal init(httpClient: HTTPClient, mapper: Mapper<JSON, Vehicle>) {
+    internal init(httpClient: HTTPClient, mapper: Mapper<JSON, Vehicle>, vehiclesURL: URL) {
         self.httpClient = httpClient
         self.mapper = mapper
+        self.vehiclesURL = vehiclesURL
     }
     
     func getVehicles(completion: @escaping ((Result<[Vehicle]>) -> Void)) {
-        
+        httpClient.performOperation(parameters: HTTPOperationParameters(url: vehiclesURL, method: .GET)) { (result) in
+            switch result {
+            case .success(let json):
+                do {
+                    let jsons: [JSON] = try json.get(key: "vehicles")
+                    let vehicles = jsons.compactMap { try? self.mapper.map(from: $0) }
+                    completion(Result.success(vehicles))
+                } catch {
+                    completion(Result.failure(error))
+                }
+            case .failure(let error):
+                completion(Result.failure(error))
+            }
+        }
     }
 }
