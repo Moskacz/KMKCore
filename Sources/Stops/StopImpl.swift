4128e89f32e4e6f7fbec82dda517ea22bfe90519
Michał Moskała <moskala.michal@gmail.com>
2018-07-03 19:49:27 +0200

    Added StopImpl

---
+++ Sources/Stops/StopImpl.swift
@@ -0,0 +1,16 @@
+//
+//  StopImpl.swift
+//  KMKCore
+//
+//  Created by Michal Moskala on 03.07.2018.
+//
+
+import Foundation
+
+internal struct StopImpl: Stop {
+    let id: String
+    let latitude: Int
+    let longitude: Int
+    let name: String
+    let shortName: String
+}
